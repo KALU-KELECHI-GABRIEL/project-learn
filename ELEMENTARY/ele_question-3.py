a34b2dd5a617e2e8175f67f544aca80af5475bd9
unknown <frakels1995@gmail.com>
2025-07-14 18:18:52 +0100

    Changes to ele_question-3.py

--- ELEMENTARY/ele_question-3.py
+++ ELEMENTARY/ele_question-3.py
@@ -1,9 +1,7 @@
 #Modify the previous program such that only the users Alice and Bob are greeted with their names.
 name = input("Please input your name: ")
 
-if name == "Alice":
-    print(f"Good Afternoon, {name}")
-elif name == "Bob":
+if name.lower() == "alice" or name.lower() == "bob":
     print(f"Good Afternoon, {name}")
 else:
     print("Good Afternoon")
